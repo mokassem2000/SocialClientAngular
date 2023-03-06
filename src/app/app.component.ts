b54f800415765f720eca8e3ef7c449dc49f1ffc6
mokassem <qasm15766@gmail.com>
2023-03-06 18:38:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from './_entites/iuser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  users$: Observable<IUser[]> | any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.users$ = this.getAllUSer();
+  }
+
   title = 'project';
+  getAllUSer() {
+    return this.http.get<IUser[]>('https://localhost:7068/api/Users');
+  }
 }
